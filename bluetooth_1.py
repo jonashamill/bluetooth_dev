8bae02278e33d1d7b54078517d391718467e6149
jay <jonas.hamill@bristol.ac.uk>
2023-06-02 15:35:24 +0100

    adding bt client and servers

--- bluetooth_1.py
+++ bluetooth_1.py
@@ -1,2 +1,25 @@
+# Server
+
+
 import bluetooth
 
+def main():
+    
+    serverSock = bluetooth.BluetoothSocket( bluetooth.RFCOMM)
+
+    port = 1 
+    serverSock.bind(("", port))
+    serverSock.listen(1)
+
+    clientSock, address = serverSock.accept()
+    print ("Accepted connection from: ", address)
+
+    data = clientSock.recv(1024)
+    print ("recieved [%s]" % data)
+
+    clientSock.close()
+    serverSock.close()
+
+
+if __name__ == '__main__':
+    main()
---
+++ bluetooth_2_c.py
@@ -0,0 +1,21 @@
+# Client
+
+
+import bluetooth
+
+def main():
+    
+    bdAddr = '3C:21:9C:E0:88:54'
+
+    port = 1
+
+    sock = bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+    sock.connect ((bdAddr, port))
+
+    sock.send ("Hello Blue World!!")
+
+    sock.close()
+
+
+if __name__ == '__main__':
+    main()
