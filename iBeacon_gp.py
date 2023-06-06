2bc5c70f3bb6f29fe7867cc629f9f212f0760bd9
jay <jonas.hamill@bristol.ac.uk>
2023-06-06 17:07:13 +0100

    updating iB

--- iBeacon_gp.py
+++ iBeacon_gp.py
@@ -11,15 +11,15 @@ def main():
     power = 200
 
     # Convert the parameters to the byte representation
-    payload = [
+    payload = bytes([
         0x02, 0x01, 0x1A,  # iBeacon advertisement indicator
         0x1A,  # Length of the remaining payload
         0xFF, 0x4C, 0x00, 0x02, 0x15,  # iBeacon identifier
-    ]
-    payload += [int(x, 16) for x in uuid.split("-")]
-    payload += [major >> 8, major & 0xFF]
-    payload += [minor >> 8, minor & 0xFF]
-    payload += [power]
+    ] + [int(x, 16) for x in uuid.split("-")] + [
+        major >> 8, major & 0xFF,
+        minor >> 8, minor & 0xFF,
+        power
+    ])
 
     service.start_advertising(payload)
 
