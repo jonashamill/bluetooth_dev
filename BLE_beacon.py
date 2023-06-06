54efa8a26d7592224ed632d0a5833f5c233407b4
jay <jonas.hamill@bristol.ac.uk>
2023-06-06 17:25:38 +0100

    becon payload upd

--- BLE_beacon.py
+++ BLE_beacon.py
@@ -6,7 +6,8 @@ from time import sleep
 def main():
     service = BeaconService()
 
-    service.start_advertising(1, 1, 1, 200, "Hello, World!")
+    service.start_advertising("3a6e4db3-4ebe-49d4-b16c-3a537deef82f",
+                          1, 1, 1, 200)
     
     input('Beacon running.')
 
