e81cde778416516ca56a16d83dbfd2aa4b0585cc
jay <jonas.hamill@bristol.ac.uk>
2023-06-06 14:51:15 +0100

    adding beacon

---
+++ BLE_beacon.py
@@ -0,0 +1,18 @@
+# BLE BEACON
+
+from bluetooth.ble import BeaconService
+from time import sleep
+
+def main():
+    service = BeaconService()
+
+    service.start_advertising("11111111-2222-3333-4444-555555555555",
+                          1, 1, 1, 200)
+    sleep(15)
+    service.stop_advertising()
+
+    print("Done.")
+
+
+if __name__ == '__main__':
+    main()
